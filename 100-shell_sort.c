71d536a52ea66f6918510f49468276c46b99e974
vick-ram <vickramodero6@gmail.com>
2024-02-24 11:52:45 -0800

    Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,6 +13,8 @@ void shell_sort(int *array, size_t size)
 	size_t gap = 1, i, j;
 	int temp;
 
+	if (size <= 1 || array == NULL)
+		return;
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
 	while (gap > 0)
